7ebf6662c32d4cbacf4e7f06cb99edfdec4fdc91
Aman Fangeria <aman.fangeria@geekyants.com>
2024-08-27 16:53:17 +0530

    Learnt modifiers like withRepeat, withSequence & withDelay

--- app/index.tsx
+++ app/index.tsx
@@ -13,6 +13,7 @@ const index = () => {
             <Text onPress={() => router.push("Animation1")}>Animation 1</Text>
             <Text onPress={() => router.push("Animation2")}>Animation 2</Text>
             <Text onPress={() => router.push("Animation3")}>Animation 3</Text>
+            <Text onPress={() => router.push("Animation4")}>Animation 4</Text>
         </View>
     );
 };
